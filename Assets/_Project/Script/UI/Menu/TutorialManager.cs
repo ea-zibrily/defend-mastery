20edeb744a426178975e33afb5e4245c71e5ca22
Zibril Yuan <zyuuannn@gmail.com>
2024-11-04 14:41:32 +0700

    feat: tutorial pagination

--- Assets/_Project/Script/UI/Menu/TutorialManager.cs
+++ Assets/_Project/Script/UI/Menu/TutorialManager.cs
@@ -12,6 +12,10 @@ namespace Defend.UI
 
         [Header("Tutorial")]
         [SerializeField] private Button closeButtonUI;
+        [SerializeField] private Button nextButtonUI;
+        [SerializeField] private Button previousButtonUI;
+
+        [Header("Reference")]
         [SerializeField] private SimpleScrollSnap scrollSnap;
 
         #endregion
@@ -20,8 +24,24 @@ namespace Defend.UI
 
         private void Start()
         {
+            // Setup scroll-snap
             scrollSnap.StartScrollSnap();
+            scrollSnap.onPanelSelected.RemoveAllListeners();
+            scrollSnap.onPanelSelected.AddListener(()=>
+            {
+                var targetPanel = scrollSnap.TargetPanel;
+                var panelNumber = scrollSnap.NumberOfPanels - 1;
+
+                previousButtonUI.gameObject.SetActive(targetPanel != 0);
+                nextButtonUI.gameObject.SetActive(targetPanel != panelNumber);
+            });
+
+            // Setup button
             closeButtonUI.onClick.AddListener(CloseTutorial);
+            nextButtonUI.onClick.AddListener(TapTutorial);
+            previousButtonUI.onClick.AddListener(TapTutorial);
+            
+            previousButtonUI.gameObject.SetActive(scrollSnap.TargetPanel != 0);
         }
 
         private void CloseTutorial()
@@ -29,6 +49,11 @@ namespace Defend.UI
             AudioManager.Instance.PlayAudio(Musics.ButtonSfx);
             scrollSnap.StartScrollSnap();
             gameObject.SetActive(false);
+        }
+        
+        private void TapTutorial()
+        {
+            AudioManager.Instance.PlayAudio(Musics.ButtonSfx);
         }
 
         #endregion
